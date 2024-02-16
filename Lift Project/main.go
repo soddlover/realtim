1606a219a9781924f9dcc026d65cb08fad2ba947
Your Name <you@example.com>
2024-02-16 11:48:57 +0100

    sui

--- Lift Project/main.go
+++ Lift Project/main.go
@@ -5,15 +5,28 @@ import (
 	"fmt"
 	"mymodule/assigner"
 	. "mymodule/elevator"
-	. "mymodule/network"
+
+	//. "mymodule/network"
+	"mymodule/network/localip"
+
 	"time"
 )
 
 func main() {
+	fmt.Println("main running.")
 
 	var id string
 	flag.StringVar(&id, "id", "", "id of this peer")
 	flag.Parse()
+	if id == "" {
+		localIP, err := localip.LocalIP()
+		if err != nil {
+			fmt.Println(err)
+			localIP = "DISCONNECTED"
+		}
+		//id = fmt.Sprintf("peer-%s-%d", localIP, os.Getpid())
+		id = localIP
+	}
 
 	world := &assigner.World{
 		Map: make(map[string]Elev),
@@ -25,12 +38,14 @@ func main() {
 		OrderComplete:  make(chan Order),
 		OrderAssigned:  make(chan Order),
 	}
-	testchan := make(chan Elev)
-	go SendRandomShitMotherFucker(testchan)
-	fmt.Print("Hello, World!")
-	go RunElev(channels)
-	go StateBroadcaster(testchan, world, id)
-	go PeerConnector(id, world)
+	assigner.TestOrderCommunication(channels, world, id)
+
+	// testchan := make(chan Elev)
+	// go SendRandomShitMotherFucker(testchan)
+	//fmt.Print("Hello, World!")
+	// go RunElev(channels)
+	// go StateBroadcaster(testchan, world, id)
+	// go PeerConnector(id, world)
 	select {}
 }
 
