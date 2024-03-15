094fdc73162f49176544cc16160c69397e7a1e37
roryf <rory.fitzgerald@ntnu.no>
2024-03-15 03:42:53 +0100

    fix

--- Lift Project/network/networkFSM.go
+++ Lift Project/network/networkFSM.go
@@ -36,6 +36,7 @@ func NetworkFSM(
 	var startOrderForwarderOnce sync.Once
 	var lastnetworkOrders [config.N_FLOORS][config.N_BUTTONS]string
 	var chosenOne bool = true
+	var latestNetworkOrderData NetworkOrdersData
 
 	requestSystemState := make(chan bool, config.NETWORK_BUFFER_SIZE)
 	systemState := make(chan map[string]Elev, config.NETWORK_BUFFER_SIZE)
@@ -99,7 +100,6 @@ func NetworkFSM(
 			time.Sleep(1 * time.Second)
 
 		case dt_wrangler:
-			var latestNetworkOrderData NetworkOrdersData
 			select {
 			case <-sheriffDead:
 				lastnetworkOrders = latestNetworkOrderData.NetworkOrders
